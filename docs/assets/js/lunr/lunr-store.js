18316382f92fc346f7761b659d3ed0bed9ba2303
D. S. Hollman <dshollm@sandia.gov>
2020-04-14 16:46:10 -0700

    moved to docs generation

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "`mdspan`: A Non-Owning Multidimensional Array Reference",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/cpp_proposals/2020-01-01-p0009/",
+        "teaser": null
+      },{
+        "title": "A Unified Executors Proposal for C++",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/cpp_proposals/2020-01-01-p0443/",
+        "teaser": null
+      },{
+        "title": "I'm trans! Call me Daisy.",
+        "excerpt":"Coming out can be an exciting, scary, exhilarating, nerve-racking, emotional time in the life of any transgender person. Pausing all social interactions to shelter in place in the middle of my coming out process because of a global pandemic has made my experience particularly interesting. While it is not my...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/posts/2020-04-14-im-trans-call-me-daisy/",
+        "teaser": null
+      },]
