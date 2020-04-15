b29524544f37d97475ba61a5ad5ba9eaac6548a0
D. S. Hollman <dshollm@sandia.gov>
2020-04-14 18:23:48 -0700

    no longer building with production because of a bug I don't feel like messing with

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "`mdspan`: A Non-Owning Multidimensional Array Reference",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "https://dsh.fyi/pages/dhollman/dsh.fyi/cpp_proposals/2020-01-01-p0009/",
+        "url": "https://dsh.fyi/cpp_proposals/2020-01-01-p0009/",
         "teaser": null
       },{
         "title": "A Unified Executors Proposal for C++",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "https://dsh.fyi/pages/dhollman/dsh.fyi/cpp_proposals/2020-01-01-p0443/",
+        "url": "https://dsh.fyi/cpp_proposals/2020-01-01-p0443/",
         "teaser": null
       },{
         "title": "I'm trans! Call me Daisy.",
         "excerpt":"Coming out can be an exciting, scary, exhilarating, nerve-racking, emotional time in the life of any transgender person. Pausing all social interactions to shelter in place in the middle of my coming out process because of a global pandemic has made my experience particularly interesting. While it is not my...","categories": [],
         "tags": [],
-        "url": "https://dsh.fyi/pages/dhollman/dsh.fyi/posts/2020-04-14-im-trans-call-me-daisy/",
+        "url": "https://dsh.fyi/posts/2020-04-14-im-trans-call-me-daisy/",
         "teaser": null
       },]
